f530c5fd30fdf4ab4bff375f5c2126c4a5310cb1
Caiooou <127060675+Caiooou@users.noreply.github.com>
2023-06-15 00:17:07 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+// Verifica o formulário de login
+document.getElementById("loginForm").addEventListener("submit", function(event) {
+    event.preventDefault(); // Impede o envio do formulário
+    // Coloque seu código de verificação de login aqui
+    alert("Login realizado com sucesso!");
+  });
+  
+  // Verifica o formulário de cadastro
+  document.getElementById("cadastroForm").addEventListener("submit", function(event) {
+    event.preventDefault(); // Impede o envio do formulário
+    // Coloque seu código de verificação de cadastro aqui
+    alert("Cadastro realizado com sucesso!");
+  });
+  
